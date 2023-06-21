6eaca29c09c14f4b1a87013cf444fd9ad275c57c
hariprakash-sparkout <hariprakash@sparkouttech.com>
2023-06-21 23:16:36 +0530

    fix

--- src/app/modules/user/slot-token/slot-token.component.ts
+++ src/app/modules/user/slot-token/slot-token.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { SnackbarServiceService } from 'app/shared/snackbar-service.service';
 import { Socket } from 'ngx-socket-io';
 interface Seat {
-  label: number;
-  selected: boolean;
-  disable:boolean
+  tokenNumber: number;
+  selectedBy: string;
+  isSelected:boolean
 }
 @Component({
   selector: 'app-slot-token',
@@ -17,16 +18,12 @@ export class SlotTokenComponent implements OnInit {
   onlineUsers: number = 0;
   isDisabled: any = []
   seats: Seat[] = [];
+  tokenData=[]
+  
   
 
-  constructor(private socket: Socket) {
-    for (let i = 0; i <= 32; i++) {
-      this.seats.push({
-        label:  i,
-        selected: false,
-        disable:true
-      });
-    }
+  constructor(private socket: Socket,private snackbar:SnackbarServiceService) {
+    
   
   }
 
@@ -38,7 +35,16 @@ export class SlotTokenComponent implements OnInit {
     this.socket.emit('getGame')
 
     this.socket.fromEvent('getGame').subscribe((message: any) => {
+      for (let i = 0; i <message.length; i++) {
+        this.seats.push({
+          tokenNumber:  i,
+          selectedBy: '',
+          isSelected:false
+        });
+        // if(this.seats[i].selectedBy)
+      }
       this.handle(message)
+      this.tokenData=message
       console.log('haha', message)
 
     });
@@ -49,27 +55,36 @@ export class SlotTokenComponent implements OnInit {
   }
 
  async handle(message:any){
+  this.seats=[]
+  for (let i =0;i<message.length;i++){
+    this.seats.push(message[i])
+  }
   
-  for (let i = 0; i <= 32; i++) {
-    this.seats[i].disable=true
-    }
+  // for (let i = 0; i <= 32; i++) {
+  //   this.seats[i].disable=true
+  //   }
   
-    for (let i = 0; i <message.length; i++) {
-      this.seats[message[i]].disable= false
-      console.log( this.seats[message[i]].disable,i)
-    }
+  //   for (let i = 0; i <message.totalToken.length; i++) {
+  //     this.seats[message.totalToken[i]].disable= false
+  //     console.log( this.seats[message.totalToken[i]].disable,i)
+  //   }
   }
 
-  toggleSeatSelection(index: number, ): void {
-    
+  toggleSeatSelection(index: number,isSelected:any ): void {
+   console.log(isSelected);
+   
+    if(isSelected){
+      this.snackbar.error(`This token is selected by ${this.seats[index-1].selectedBy}`,4000)
+      return
+    }
     let token = localStorage.getItem('accessToken')
     let message = {
       token: token,
-      message: index
+      index: index
     }
 
-    this.seats[index].selected = !this.seats[index].selected;
-    console.log('Seat ' + (index + 1) + ' selected:', this.seats[index].selected);
+    // this.seats[index].selected = !this.seats[index].selected;
+    // console.log('Seat ' + (index + 1) + ' selected:', this.seats[index].selected);
     this.socket.emit('chat', message);
   }
 }
