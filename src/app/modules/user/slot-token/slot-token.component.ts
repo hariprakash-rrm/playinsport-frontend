f073f2509505e5181a28def7012c8ec4803c8f01
hariprakash-sparkout <hariprakash@sparkouttech.com>
2023-06-21 09:19:49 +0530

    home

--- src/app/modules/user/slot-token/slot-token.component.ts
+++ src/app/modules/user/slot-token/slot-token.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Socket } from 'ngx-socket-io';
 interface Seat {
-  label: string;
+  label: number;
   selected: boolean;
 }
 @Component({
@@ -13,25 +13,39 @@ export class SlotTokenComponent implements OnInit {
   message: string;
   messages: string[] = [];
   onlineUsers: number = 0;
+  isDisabled: any = []
   seats: Seat[] = [];
+  test:any=[]
   constructor(private socket: Socket) {
     for (let i = 1; i <= 32; i++) {
       this.seats.push({
-        label: 'Seat ' + i,
+        label:  i,
         selected: false,
       });
+      this.test.push(i)
+      this.isDisabled.push(false)
     }
   }
 
   ngOnInit() {
-    this.socket.fromEvent('chat').subscribe((message: string) => {
+    this.socket.emit('getGame')
+    this.socket.fromEvent('chat').subscribe((message: any) => {
       this.messages.push(message);
       console.log(message)
+      for (let i = 0; i <message.length; i++) {
+        let index = this.test.indexOf(message[i])
+        console.log(index)
+        if (index) {
+          this.isDisabled[i] = true
+        }
+      }
+      console.log(this.isDisabled)
     });
 
-    this.socket.fromEvent('getGame').subscribe((message: string) => {
+    this.socket.fromEvent('getGame').subscribe((message: any) => {
 
-      console.log(message)
+      console.log('haha', message)
+
     });
 
     this.socket.fromEvent('users').subscribe((users: number) => {
@@ -39,7 +53,8 @@ export class SlotTokenComponent implements OnInit {
     });
   }
 
-  toggleSeatSelection(index: number): void {
+  toggleSeatSelection(index: number, ): void {
+    
     let token = localStorage.getItem('accessToken')
     let message = {
       token: token,
