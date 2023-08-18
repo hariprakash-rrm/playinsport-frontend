99ed240e44ecb3736fe6c54f43ce11be29abbef7
hariprakash-sparkout <hariprakash@sparkouttech.com>
2023-08-18 17:02:37 +0530

    fixes

--- src/app/modules/auth/sign-in/sign-in.component.ts
+++ src/app/modules/auth/sign-in/sign-in.component.ts
@@ -49,6 +49,7 @@ export class AuthSignInComponent implements OnInit, OnDestroy {
     tokens: any;
     setPasswordForm: FormGroup;
     isAdmin:boolean;
+    isOtpSent:boolean
 
     phoneNumber: any;
     /**
@@ -186,7 +187,10 @@ export class AuthSignInComponent implements OnInit, OnDestroy {
             const data = {
             number: this.numberForm.value.numbers,
         };
-       
+       if(this.isOtpSent){
+        this.errorMessage=`Please wait ${this.countdown} seconds and try again`
+        return
+       }
         this._authService.forgotPassword(data).subscribe(
             (response) => {
                 if (response.statusCode === 201) {
@@ -195,6 +199,7 @@ export class AuthSignInComponent implements OnInit, OnDestroy {
                     this.phoneNumber = this.numberForm.value.numbers;
                     this.startCountdown();
                     this.errorMessage = '';
+                    this.isOtpSent=true
                 }
             },
             (error: HttpErrorResponse) => {
@@ -212,15 +217,22 @@ export class AuthSignInComponent implements OnInit, OnDestroy {
         this.currentStep++;
     }
 
+    goBack(){
+        this.errorMessage=''
+        this.currentStep--
+    }
+
     startCountdown(): void {
+        this.countdown=45
         this.errorMessage = '';
 
         // this.countdown = 10;
         this.interval = setInterval(() => {
             this.countdown--;
-            if (this.countdown === 0) {
+            if (this.countdown <= 0) {
                 clearInterval(this.interval);
                 this.countdown = 0;
+                this.isOtpSent=false
             }
             console.log(this.countdown);
         }, 1000); // Changed interval to 1000 milliseconds (1 second)
@@ -262,7 +274,7 @@ export class AuthSignInComponent implements OnInit, OnDestroy {
             this.errorMessage =
                 'Please wait for 45 seconds before generating a new OTP.';
         }
-        if (this.countdown === 0) {
+        if (this.countdown <= 0) {
             // console.log('GOTOSENDOTP');
             this.currentStep--;
             this.countdown = 45;
