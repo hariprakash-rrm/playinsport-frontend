b6b76535cbf015bd3c348f38d7cd851141c1de5c
hariprakash-sparkout <hariprakash@sparkouttech.com>
2023-08-19 09:01:21 +0530

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
     production: false,
-    apiUrl: 'http://16.170.146.34:3000',
+    apiUrl: 'http://157.245.99.18:3000',
     // apiUrl: 'http://localhost:3000'
 };
 
