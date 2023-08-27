4eb7b90531ce74abb5fb8264be5d2b0855207fc3
hariprakash-sparkout <hariprakash@sparkouttech.com>
2023-08-27 18:38:44 +0530

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
     production: true,
-    apiUrl: 'http://157.245.99.18:3000',
+    apiUrl: 'http://teamquantum.in:3000',
     // apiUrl:'http://localhost:3000'
 };
 
