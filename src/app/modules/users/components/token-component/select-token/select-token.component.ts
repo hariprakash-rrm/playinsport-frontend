8d89ebf6fde4b03ba80e2d260f44d344924e6da7
hariprakash-sparkout <hariprakash@sparkouttech.com>
2023-09-09 18:30:58 +0530

    fixes

--- src/app/modules/users/components/token-component/select-token/select-token.component.ts
+++ src/app/modules/users/components/token-component/select-token/select-token.component.ts
@@ -1,219 +1,214 @@
-import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import {
+    Component,
+    EventEmitter,
+    Input,
+    OnDestroy,
+    OnInit,
+    Output,
+} from '@angular/core';
 import { Router } from '@angular/router';
 import { SnackbarServiceService } from 'app/shared/snackbar-service.service';
 import { Socket } from 'ngx-socket-io';
 
-
 interface Seat {
-  round: any;
-  tokenNumber: number;
-  selectedBy: string;
-  isSelected: boolean;
-  userSelected: boolean;
+    round: any;
+    tokenNumber: number;
+    selectedBy: string;
+    isSelected: boolean;
+    userSelected: boolean;
 }
 @Component({
-  selector: 'app-select-token',
-  templateUrl: './select-token.component.html',
-  styleUrls: ['./select-token.component.css']
-
+    selector: 'app-select-token',
+    templateUrl: './select-token.component.html',
+    styleUrls: ['./select-token.component.css'],
 })
 export class SelectTokenComponent implements OnInit, OnDestroy {
+    @Input() gameId: number;
+    @Input() youtubeLink: any;
+    @Input() youtubeLiveLink: string;
+    @Input() facebookLink: string;
+    @Input() facebookLiveLink: string;
+    @Output() activity: EventEmitter<string> = new EventEmitter<string>();
+    message: string;
+    messages: string[] = [];
+    onlineUsers: number = 0;
+    isDisabled: any = [];
+    seats: Seat[] = [];
+    tokenData = [];
+    userDetails: any;
+    round: number = 1;
+    viewSelected: boolean;
 
-  @Input() gameId: number;
-  @Output() activity: EventEmitter<string> = new EventEmitter<string>();
-  message: string;
-  messages: string[] = [];
-  onlineUsers: number = 0;
-  isDisabled: any = [];
-  seats: Seat[] = [];
-  tokenData = [];
-  userDetails: any;
-  round: number = 1;
-  viewSelected: boolean
-  @Input() youtubeLink: string;
-  @Input() youtubeLiveLink: string;
-  @Input() facebookLink: string;
-  @Input() facebookLiveLink: string;
+    constructor(
+        private socket: Socket,
+        private snackbar: SnackbarServiceService,
+        private router: Router
+    ) {}
 
+    /**
+     * Calls when page loads
+     */
+    async ngOnInit() {
+        this.viewSelected = false;
+        this.round = this.gameId;
+        let user = await localStorage.getItem('user');
+        this.userDetails = JSON.parse(user);
 
-  constructor(
-    private socket: Socket,
-    private snackbar: SnackbarServiceService,
-    private router: Router
-  ) { }
+        this.triggerSocket();
+        console.log(this.youtubeLink);
+        console.log('facebooklink', this.facebookLink, this.gameId);
+    }
 
-  /**
-   * Calls when page loads
-   */
-  async ngOnInit() {
-    this.viewSelected = false
-    this.round = this.gameId
-    let user = await localStorage.getItem('user');
-    this.userDetails = JSON.parse(user);
-
-    this.triggerSocket();
-    // console.log(this.youtubeLink);
-    // console.log('facebooklink', this.facebookLink);
-
-
-  }
-
-  /**
-   * triggering socket connection
-   */
-  async triggerSocket() {
-    this.socket.on('connect', async () => {
-      const socketId = this.socket.ioSocket.id;
-      this.userDetails.socketId = await socketId;
-      this.handleSocketResponse();
-    });
-    this.socket.connect()
-    await this.socket.on('disconnect', () => {
-    });
-  }
-
-
-  /**
-   * 
-   * @param message 
-   * @returns 
-   */
-  handleSeats(message: any) {
-    // // console.log(message)
-    if (+this.round == +message[0].round) {
-      for (let i = 0; i < message.length; i++) {
-        this.seats.push({
-          round: this.round,
-          tokenNumber: i,
-          selectedBy: '',
-          isSelected: false,
-          userSelected: false,
+    /**
+     * triggering socket connection
+     */
+    async triggerSocket() {
+        this.socket.on('connect', async () => {
+            const socketId = this.socket.ioSocket.id;
+            this.userDetails.socketId = await socketId;
+            this.handleSocketResponse();
         });
-      }
-      this.handleSeatData(message);
-      this.tokenData = message;
-    } else {
-      return;
+        this.socket.connect();
+        await this.socket.on('disconnect', () => {});
     }
-  }
 
-  /**
-   * 
-   * @param message 
-   */
-  async handleSeatData(message: any) {
-    this.seats = [];
-    for (let i = 0; i < message.length; i++) {
-      this.seats.push(message[i]);
-    }
-    for (let i = 0; i < message.length; i++) {
-      // // console.log(this.userDetails.username, this.seats[i].selectedBy)
-      if (this.userDetails.name == this.seats[i].selectedBy) {
-        this.seats[i].userSelected = true;
-      }
-    }
-  }
-
-  /**
-   * handling socket respose - error , game , balance etc
-   */
-  handleSocketResponse() {
-    let token = localStorage.getItem('accessToken');
-    this.socket.fromEvent('userBalance').subscribe((message: any) => {
-      // console.log('token', message);
-
-    });
-
-    this.socket.fromEvent('isError').subscribe((message: any) => {
-      this.snackbar.error(message?.message, 3000);
-
-    });
-
-    this.socket.fromEvent('game').subscribe((message: any) => {
-      this.handleSeats(message);
-
-      // console.log('handleseat', message);
-    });
-
-    this.socket.emit('getGame', this.round);
-    this.socket.emit('game', this.round);
-    let balanceQuery = {
-      token: token,
-      userId: this.userDetails.socketId,
-    };
-    this.socket.emit('userBalance', balanceQuery);
-    this.socket.fromEvent('getGame').subscribe((message: any) => {
-      if (+this.round == +message.round) {
-        this.seats[message.tokenNumber - 1] = message;
-        if (
-          this.userDetails.name ==
-          this.seats[message.tokenNumber - 1].selectedBy
-        ) {
-          this.seats[message.tokenNumber - 1].userSelected = true;
+    /**
+     *
+     * @param message
+     * @returns
+     */
+    handleSeats(message: any) {
+        // // console.log(message)
+        if (+this.round == +message[0].round) {
+            for (let i = 0; i < message.length; i++) {
+                this.seats.push({
+                    round: this.round,
+                    tokenNumber: i,
+                    selectedBy: '',
+                    isSelected: false,
+                    userSelected: false,
+                });
+            }
+            this.handleSeatData(message);
+            this.tokenData = message;
+        } else {
+            return;
         }
-
-      } else {
-        return;
-      }
-    });
-
-    this.socket.fromEvent('users').subscribe((users: number) => {
-      this.onlineUsers = users;
-    });
-  }
-
-  /**
-   * Selecting tokens
-   * @param index 
-   * @param isSelected 
-   * @returns 
-   */
-  toggleSeatSelection(index: number, isSelected: boolean): void {
-    if (isSelected) {
-      this.snackbar.error(
-        `This token is selected by ${this.seats[index - 1].selectedBy}`,
-        4000
-      );
-      return;
-    }
-    let token = localStorage.getItem('accessToken');
-    let message = {
-      token: token,
-      index: index - 1,
-      tokenNumber: index,
-      id: this.userDetails.socketId,
-      round: this.round,
-    };
-    this.socket.emit('chat', message);
-  }
-
-  /**
-   * change game or date it will destroy this page
-   */
-  async changeGame() {
-    this.activity.emit();
-    this.socket.disconnect();
-  }
-
-  ngOnDestroy(): void {
-    this.socket.disconnect();
-  }
-
-  /**
-   * viewing selected tokens
-   */
-  viewSelectedTokens() {
-    this.viewSelected = !this.viewSelected
-  }
-  closeSelectedTokens() {
-    this.viewSelected = false;
-  }
-
-  onLinkSelected(selectedLink: string) {
-    if (selectedLink) {
-      window.open(selectedLink, '_blank');
     }
 
-  }
+    /**
+     *
+     * @param message
+     */
+    async handleSeatData(message: any) {
+        this.seats = [];
+        for (let i = 0; i < message.length; i++) {
+            this.seats.push(message[i]);
+        }
+        for (let i = 0; i < message.length; i++) {
+            // // console.log(this.userDetails.username, this.seats[i].selectedBy)
+            if (this.userDetails.name == this.seats[i].selectedBy) {
+                this.seats[i].userSelected = true;
+            }
+        }
+    }
 
+    /**
+     * handling socket respose - error , game , balance etc
+     */
+    handleSocketResponse() {
+        let token = localStorage.getItem('accessToken');
+        this.socket.fromEvent('userBalance').subscribe((message: any) => {
+            // console.log('token', message);
+        });
+
+        this.socket.fromEvent('isError').subscribe((message: any) => {
+            this.snackbar.error(message?.message, 3000);
+        });
+
+        this.socket.fromEvent('game').subscribe((message: any) => {
+            this.handleSeats(message);
+
+            // console.log('handleseat', message);
+        });
+
+        this.socket.emit('getGame', this.round);
+        this.socket.emit('game', this.round);
+        let balanceQuery = {
+            token: token,
+            userId: this.userDetails.socketId,
+        };
+        this.socket.emit('userBalance', balanceQuery);
+        this.socket.fromEvent('getGame').subscribe((message: any) => {
+            console.log(message);
+            if (+this.round == +message.round) {
+                this.seats[message.tokenNumber - 1] = message;
+                if (
+                    this.userDetails.name ==
+                    this.seats[message.tokenNumber - 1].selectedBy
+                ) {
+                    this.seats[message.tokenNumber - 1].userSelected = true;
+                }
+            } else {
+                return;
+            }
+        });
+
+        this.socket.fromEvent('users').subscribe((users: number) => {
+            this.onlineUsers = users;
+        });
+    }
+
+    /**
+     * Selecting tokens
+     * @param index
+     * @param isSelected
+     * @returns
+     */
+    toggleSeatSelection(index: number, isSelected: boolean): void {
+        if (isSelected) {
+            this.snackbar.error(
+                `This token is selected by ${this.seats[index - 1].selectedBy}`,
+                4000
+            );
+            return;
+        }
+        let token = localStorage.getItem('accessToken');
+        let message = {
+            token: token,
+            index: index - 1,
+            tokenNumber: index,
+            id: this.userDetails.socketId,
+            round: this.round,
+        };
+        this.socket.emit('chat', message);
+    }
+
+    /**
+     * change game or date it will destroy this page
+     */
+    async changeGame() {
+        this.activity.emit();
+        this.socket.disconnect();
+    }
+
+    ngOnDestroy(): void {
+        this.socket.disconnect();
+    }
+
+    /**
+     * viewing selected tokens
+     */
+    viewSelectedTokens() {
+        this.viewSelected = !this.viewSelected;
+    }
+    closeSelectedTokens() {
+        this.viewSelected = false;
+    }
+
+    onLinkSelected(selectedLink: string) {
+        if (selectedLink) {
+            window.open(selectedLink, '_blank');
+        }
+    }
 }
--- src/app/modules/users/components/token-component/token/token.component.ts
+++ src/app/modules/users/components/token-component/token/token.component.ts
@@ -3,110 +3,112 @@ import { SnackbarServiceService } from 'app/shared/snackbar-service.service';
 import { TokenService } from '../../token.service';
 
 @Component({
-  selector: 'app-token',
-  templateUrl: './token.component.html',
-  styleUrls: ['./token.component.css']
+    selector: 'app-token',
+    templateUrl: './token.component.html',
+    styleUrls: ['./token.component.css'],
 })
 export class TokenComponent implements OnInit {
+    gameId: any = null;
+    youtubeLink: any = null;
+    youtubeLiveLink: string = null;
+    facebookLink: string = null;
+    facebookLiveLink: string = null;
 
+    selectedTime: any;
+    selectedTab: any = 'live';
+    selectedDate: Date;
 
-  gameId: any = null
-  youtubeLink: string = null;
-  youtubeLiveLink: string = null;
-  facebookLink: string = null;
-  facebookLiveLink: string = null;
+    defaultValue = { hour: 13, minute: 30 };
+    games: any;
+    prizes: any;
 
-  selectedTime: any;
-  selectedTab:any='live'
-  selectedDate: Date;
+    winner: string = 'Not yet updated';
+    winners: any;
+    viewPrize: boolean = false;
 
-  defaultValue = { hour: 13, minute: 30 };
-  games: any
-  prizes:any
+    timeChangeHandler(event: any) {}
 
-  winner: string = 'Not yet updated'
-  winners: any;
-  viewPrize:boolean=false
+    invalidInputHandler() {}
 
+    constructor(
+        private snackbar: SnackbarServiceService,
+        private service: TokenService
+    ) {
+        // this.selectedDate = new Date();
+    }
 
-  timeChangeHandler(event: any) { }
+    async ngOnInit() {
+        this.getGame();
+    }
 
-  invalidInputHandler() { }
+    viewPrizes(card: any) {
+        console.log(card);
+        this.prizes = card.prize;
+        this.viewPrize = true;
+    }
+    CloseModal() {
+        this.viewPrize = false;
+    }
 
+    onDateChange(event: any) {
+        // console.log('Selected Date:', this.selectedDate);
 
-  constructor(private snackbar: SnackbarServiceService, private service: TokenService,) {
+        this.getGame();
+    }
 
-    // this.selectedDate = new Date();
-  }
+    setData(card: any) {
+        this.gameId = card?.round;
+        this.youtubeLink = card?.youtubeLink;
+        this.youtubeLiveLink = card?.youtubeLiveLink;
+        this.facebookLink=card?.facebookLink;
+        this.facebookLiveLink=card?.facebookLiveLink
+    }
 
-  async ngOnInit() {
-    this.getGame()
-  }
+    getGame() {
+        //testing purpose
+        // console.log('trigger')
+        let tDate = 'Sat Sep 2 2023 00:00:00 GMT+0530 (India Standard Time)';
+        this.service.getGames(tDate).subscribe(
+            (response) => {
+                if (response.statusCode === 201) {
+                    this.snackbar.success(response.message, 4000);
+                    this.games = response.data.data;
+                    this.youtubeLink = response.data.data.youtubeLink;
+                    this.youtubeLiveLink = response.data.data.youtubeLiveLink;
+                    this.facebookLink = response.data.data.facebookLink;
+                    this.facebookLiveLink = response.data.data.facebookLiveLink;
+                    this.winners = response.data.data.winnerList;
+                    console.log(response.data.data);
+                }
+            },
+            (error) => {
+                this.games = [];
+                this.snackbar.error(error.error.message, 4000);
+            }
+        );
+    }
 
-  viewPrizes(card:any){
-    console.log(card)
-    this.prizes=card.prize
-    this.viewPrize= true
-  }
-  CloseModal(){
-    this.viewPrize=false
-  }
+    activity() {
+        // // console.log('triggered',this.gameId)
 
+        // Handle the emitted event here
+        this.gameId = null;
+        // Perform any other actions with the token value
+        this.youtubeLink = null;
+        this.youtubeLiveLink = null;
+        this.facebookLink = null;
+        this.facebookLiveLink = null;
+    }
 
-  onDateChange(event: any) {
-    // console.log('Selected Date:', this.selectedDate);
-
-    this.getGame()
-  }
-
-  getGame() {
-    //testing purpose
-    // console.log('trigger')
-    let tDate='Sat Sep 2 2023 00:00:00 GMT+0530 (India Standard Time)'
-    this.service.getGames(tDate).subscribe(
-      (response) => {
-        if (response.statusCode === 201) {
-          this.snackbar.success(response.message, 4000);
-          this.games = response.data.data
-          this.youtubeLink = response.data.data.youtubeLink;
-          this.youtubeLiveLink = response.data.data.youtubeLiveLink;
-          this.facebookLink = response.data.data.facebookLink;
-          this.facebookLiveLink = response.data.data.facebookLiveLink;
-          this.winners = response.data.data.winnerList
-          // console.log(response.data.data);
-        }
-      },
-      (error) => {
-        this.games = []
-        this.snackbar.error(error.error.message, 4000);
-      }
-    );
-  }
-
-  activity() {
-    // // console.log('triggered',this.gameId)
-
-    // Handle the emitted event here
-    this.gameId = null
-    // Perform any other actions with the token value
-    this.youtubeLink = null;
-    this.youtubeLiveLink = null;
-    this.facebookLink = null;
-    this.facebookLiveLink = null;
-  }
-
-  getLink(roundNumber: number) {
-    this.games.forEach((element: any) => {
-      if (element.round == roundNumber) {
-        // console.log(element);
-        this.youtubeLiveLink = element?.youtubeLiveLink;
-        this.youtubeLink = element?.youtubeLink;
-        this.facebookLiveLink = element?.facebookLiveLink;
-        this.facebookLink = element?.facebookLink;
-      }
-    });
-  }
-
-  
-
+    // getLink(roundNumber: number) {
+    //   this.games.forEach((element: any) => {
+    //     if (element.round == roundNumber) {
+    //       // console.log(element);
+    //       this.youtubeLiveLink = element?.youtubeLiveLink;
+    //       this.youtubeLink = element?.youtubeLink;
+    //       this.facebookLiveLink = element?.facebookLiveLink;
+    //       this.facebookLink = element?.facebookLink;
+    //     }
+    //   });
+    // }
 }
