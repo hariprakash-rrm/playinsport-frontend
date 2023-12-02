17bcad2f1d253f4e22fdb34bbd247477eb59f759
priyanka-sparkout <priyanka@sparkouttech.com>
2023-12-02 21:40:54 +0530

    Crackers responsive and form component fixes

--- src/app/modules/users/components/crackers/crackers.component.ts
+++ src/app/modules/users/components/crackers/crackers.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crackers.component.scss']
 })
 export class CrackersComponent implements OnInit {
-
+  public isMobileToggled = false; 
   constructor() { }
 
   ngOnInit(): void {
